b4b95744a4e7fc18cda1509f02f3d07544e4642f
Akalanka Dias <101487259+AKALANKA28@users.noreply.github.com>
2024-12-27 18:50:07 +0530

    update

--- client/src/Website/Home/Partners/Partners.jsx
+++ client/src/Website/Home/Partners/Partners.jsx
@@ -1,37 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
-import './partners.css'
+import "./partners.css";
 
-import img1 from '../../assets/keellslogo.png'
-import img2 from '../../assets/Glomark.png'
-import img3 from '../../assets/MDlogo.png'
-import img4 from '../../assets/foodcity.png'
-import img5 from '../../assets/arpico.png'
-
+import img1 from "../../assets/keellslogo.png";
+import img2 from "../../assets/Glomark.png";
+import img3 from "../../assets/MDlogo.png";
+import img4 from "../../assets/foodcity.png";
+import img5 from "../../assets/arpico.png";
 
 const Partners = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Dynamically determine if the viewport is mobile
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Mobile threshold
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className='section section_partners'>
+    <div className="section section_partners">
       <div className="container d-flex align-items-center justify-content-center">
         <div className="row text-center">
-            <div className="col-12">
-                {/* <h6 className='heading'>What We Offer</h6> */}
-                <p className='subheading'><span>our</span> Partners</p>
-            </div>
-            <div className="col-lg-12 logo-col flex_center">
+          <div className="col-12">
+            <p className="subheading">
+              <span>our</span> Partners
+            </p>
+          </div>
+          <div className="col-lg-12 logo-col flex_center">
+            {isMobile ? (
+              <Swiper
+                spaceBetween={90} // Adjust gap between slides
+                slidesPerView={3} // Display 3 logos at a time
+                loop={true}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+                pagination={{ clickable: true }}
+              >
+                <SwiperSlide>
+                  <img src={img1} alt="Keells Logo" className="img1" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={img2} alt="Glomark Logo" className="img2" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={img3} alt="MD Logo" className="img3" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={img4} alt="Food City Logo" className="img4" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src={img5} alt="Arpico Logo" className="img5" />
+                </SwiperSlide>
+              </Swiper>
+            ) : (
               <div className="row text-align-center">
-
-                <img src={img1} alt='img1' className='col-md-4 px-lg-4 img1'></img>
-                <img src={img2} alt='img1' className='col-md-4 px-lg-4  img2'></img>
-                <img src={img3} alt='img1' className='col-md-4 img3'></img>
-                <img src={img4} alt='img1' className='col-md-4 px-lg-4 img4'></img>
-                <img src={img5} alt='img1' className='col-md-4 px-lg-4 img5'></img>
+                <img
+                  src={img1}
+                  alt="Keells Logo"
+                  className="img1 col-md-4 px-lg-4"
+                />
+                <img
+                  src={img2}
+                  alt="Glomark Logo"
+                  className="img2 col-md-4 px-lg-4"
+                />
+                <img src={img3} alt="MD Logo" className="img3 col-md-4" />
+                <img
+                  src={img4}
+                  alt="Food City Logo"
+                  className="img4 col-md-4 px-lg-4"
+                />
+                <img
+                  src={img5}
+                  alt="Arpico Logo"
+                  className="img5 col-md-4 px-lg-4"
+                />
               </div>
-            </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Partners
+export default Partners;
